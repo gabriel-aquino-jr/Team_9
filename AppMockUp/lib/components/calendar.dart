33a77f60f172b117a01d3c321193bde0343a2030
Kallcott <KeenanAllcott@live.ca>
2022-06-08 20:12:42 -0300

    calender accepts one event per day

--- AppMockUp/lib/components/calendar.dart
+++ AppMockUp/lib/components/calendar.dart
@@ -9,7 +9,7 @@ import 'package:mockup/model/schedule_model.dart';
 import 'package:mockup/utilities/palette.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-//#region Utilities
+//#region Utilitie  s
 class Event {
   final String title;
   const Event(this.title);
@@ -31,26 +31,48 @@ final calEvents = LinkedHashMap<DateTime, List<Event>>(
   hashCode: getHashCode,
 )..addAll(_calEventSource);
 
-final _calEventSource = Map.fromIterable(
-)
+final _calEventSource = Map<DateTime, List<Event>>.fromIterable(_events);
 
-final _calEventSource = 
-  Map.fromIterable(
-    
-    List.generate(50, (index) => index),
-    
-    key: (item) => DateTime.utc(calFirstDay.year, calFirstDay.month, item * 5),
-    
-    value: (item) => List.generate(
-    
-        item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-  
-  ..addAll({
-    calToday: [
-      const Event('Today\'s Event 1'),
-      const Event('Today\'s Event 2'),
-    ],
-  });
+// Query Events
+List<Schedules> _schedules = [];
+void _queryAllEvents() async {
+  dbhelper.database;
+  final allRows = await dbhelper.queryAllRows(dbhelper.database);
+  debugPrint('query all employee rows:');
+  for (var row in allRows) {
+    _schedules.add(Schedules.fromMap(row));
+  }
+  _toCalenderEvents();
+  allRows.forEach(print);
+}
+
+List _events = [];
+void _toCalenderEvents() {
+  for (var s in _schedules) {
+    final entry = <DateTime, Event>{
+      s.dateTime!: Event("${s.type} - ${s.location}")
+    };
+
+    _events.add(entry.entries);
+  }
+}
+// final _calEventSource =
+//   Map.fromIterable(
+
+//     List.generate(50, (index) => index),
+
+//     key: (item) => DateTime.utc(calFirstDay.year, calFirstDay.month, item * 5),
+
+//     value: (item) => List.generate(
+
+//         item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
+
+//   ..addAll({
+//     calToday: [
+//       const Event('Today\'s Event 1'),
+//       const Event('Today\'s Event 2'),
+//     ],
+//   });
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
 }
@@ -74,31 +96,11 @@ class _CalendarState extends State<Calendar> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
-// Query Events
-List<Schedules> _schedules = [];
-void _queryAllEvents() async{
-  dbhelper.database;
-  final allRows = await dbhelper.queryById();
-  debugPrint('query all employee rows:');
-  for (var row in allRows) {
-    _schedules.add(Schedules.fromMap(row));
-  }
-
-  setState((){});
-  allRows.forEach(print);
-}
-
-Map<DateTime, Event> _events = [];
-void _toCalenderEvents(){
-  for (var s in _schedules) {
-    
-  }
-}
-
   @override
   void initState() {
     super.initState();
     _selectedDay = _focusedDay;
+
     //Selects events for today
     _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
   }
